a4914142d31815ca1f17c4d5a7a8f5a60ff58fc4
Ralf Bonnick <ralfbonnick@gmx.net>
2022-10-30 23:18:04 +0100

    Finish Iteration3

--- app.js
+++ app.js
@@ -17,17 +17,39 @@ const spotifyApi = new SpotifyWebApi({
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET
 });
-
+//console.log(process.env.CLIENT_ID, process.env.CLIENT_SECRET)
 // Retrieve an access token
 spotifyApi
   .clientCredentialsGrant()
-  .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+  .then(data => {
+    spotifyApi.setAccessToken(data.body['access_token'])})
   .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
 // Our routes go here:
 app.get("/", (req, res) => {
   res.render("home");
 });
+
+app.get("/artist-search", async (req, res)=>{
+    const search = req.query;
+    console.log(search.artist)
+    spotifyApi
+    .searchArtists(search.artist)
+    .then(data => {
+    //console.log('The received data from the API: ', data.body);
+    // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+    const info = data.body.artists.items
+    console.log(info[0]);
+    res.render('artist-search', {info})    
+     })
+    .catch(err => console.log('The error while searching artists occurred: ', err));
+
+    
+})
+
+
+
+
 
 app.listen(3000, () =>
   console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊")
---
+++ test.js
@@ -0,0 +1,38 @@
+<h1><%- info[0].name %></h1>
+<h2><%- info[0].images[0].url %></h2>
+
+{
+    external_urls: { spotify: 'https://open.spotify.com/artist/3WrFJ7ztbogyGnTHbHJFl2' },
+    followers: { href: null, total: 23782003 },
+    genres: [
+      'beatlesque',
+      'british invasion',
+      'classic rock',
+      'merseybeat',
+      'psychedelic rock',
+      'rock'
+    ],
+    href: 'https://api.spotify.com/v1/artists/3WrFJ7ztbogyGnTHbHJFl2',
+    id: '3WrFJ7ztbogyGnTHbHJFl2',
+    images: [
+      {
+        height: 640,
+        url: 'https://i.scdn.co/image/ab6761610000e5ebe9348cc01ff5d55971b22433',
+        width: 640
+      },
+      {
+        height: 320,
+        url: 'https://i.scdn.co/image/ab67616100005174e9348cc01ff5d55971b22433',
+        width: 320
+      },
+      {
+        height: 160,
+        url: 'https://i.scdn.co/image/ab6761610000f178e9348cc01ff5d55971b22433',
+        width: 160
+      }
+    ],
+    name: 'The Beatles',
+    popularity: 83,
+    type: 'artist',
+    uri: 'spotify:artist:3WrFJ7ztbogyGnTHbHJFl2'
+  }
